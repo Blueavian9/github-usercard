7311e4424ec9f8a1ba49da47c9dfeb4439678b94
cesar.aguilar <Blueavian9@gmail.com>
2022-07-14 22:45:15 -0700

    initial

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,16 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+axios.get('https://api.github.com/users/Blueavian9')
+.then( resp => {
+  document.querySelector('.cards').appendChild(GithubAccount(resp));
+})
+.catch(() => console.log('catch'));
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,7 +36,16 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['https://api.github.com/users/bigknell', 'https://api.github.com/users/justsml', 'https://api.github.com/users/dustinmyers', 
+'https://api.github.com/users/tetondan'];
+
+followersArray.forEach(element => {
+  axios.get(element)
+  .then( resp => {
+    document.querySelector('.cards').appendChild(GithubAccount(resp));
+  })
+  .catch(() => console.log('Error'));
+})
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +66,52 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function GithubAccount(info) {
+  // element creations
+  let divCard = document.createElement('div');
+  let imgUrl = document.createElement('img');
+  let divInfo = document.createElement('div');
+  let h3Name = document.createElement('h3');
+  let pUser = document.createElement('p');
+  let pLocation = document.createElement('p');
+  let pProfile = document.createElement('p');
+  let aAddress = document.createElement('a');
+  let pFollowers = document.createElement('p');
+  let pFollowing = document.createElement('p');
+  let pBio = document.createElement('p');
+  // attribute setting
+  divCard.setAttribute('class', 'card');
+  imgUrl.setAttribute('src', `${info.data.avatar_url}`);
+  divInfo.setAttribute('class', 'card-info');
+  h3Name.setAttribute('class', 'name');
+  pUser.setAttribute('class', 'username');
+  aAddress.setAttribute('href', `${info.data.html_url}`);
+  
+  h3Name.innerHTML = `${info.data.name}`;
+  pUser.innerHTML = `${info.data.login}`;
+  pLocation.innerHTML = `Location: ${info.data.location}`;
+  pProfile.innerHTML = `Profile: `; pProfile.appendChild(aAddress);
+  aAddress.innerHTML = `${info.data.html_url}`;
+  pFollowers.innerHTML = `Followers: ${info.data.followers}`;
+  pFollowing.innerHTML = `Following: ${info.data.following}`;
+  pBio.innerHTML = `Bio: ${info.data.bio}`;
+  // appending all elements
+  const level1 = [imgUrl, divInfo];
+  const level2 = [h3Name, pUser, pLocation, pProfile, pFollowers, pFollowing, pBio];
+  for (let index = 0; index < level1.length; index++) {
+    divCard.appendChild(level1[index]);
+    if (index === 1) {
+      for (let index2 = 0; index2 < level2.length; index2++) {
+        divInfo.appendChild(level2[index2]);
+      }
+    }
+  }
+  pProfile.appendChild(aAddress);
+  return divCard;
+}
+
+
 
 /*
   List of LS Instructors Github username's:
